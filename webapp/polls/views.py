2d2add6b49bf08b5207782ac06e9c319c17379f2
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-07-30 20:36:34 +0100

    display install status for geopy on app page

--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse
 
 
+def is_geopy_installed():
+    """Returns True if geopy is installed otherwise false.
+    """
+    try:
+        import geopy
+        return True
+    except ModuleNotFoundError:
+        return False
+
+
 def index(request):
-    data = """
+    is_installed = is_geopy_installed()
+    data = f"""
         <div style="text-align: center; padding: 100px">
             <h1>Hello, World!</h1>
             <p>
                 You're at the polls app index. <br/>
                 The setup started up successfully!
+            </p>
+            <p> ... </p>
+            <p>
+                Is geopy installed? {is_installed}
             </p>
         </did>
     """
