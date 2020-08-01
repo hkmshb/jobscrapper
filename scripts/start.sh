5c29b7d44a8d3e3936ef37e10fdf9c2f4d4fe404
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-08-01 18:59:42 +0100

    update dockerfiles for webapp to include and use wait-for script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo ">> waiting on postgres"
+/app/wait-for.sh ${DJAPP_DBHOST}:${DJAPP_DBPORT} -- echo "postgres is up!"
+
 # run the django application
 python webapp/manage.py runserver 0.0.0.0:8000
