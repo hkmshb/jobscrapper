3cf123fa4bd8b83af3afa6e9740b035580b5328e
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-08-18 03:56:50 +0100

    add pagination for openings listing

--- webapp/jobs/views.py
+++ webapp/jobs/views.py
@@ -1,18 +1,50 @@
-from django.http import HttpRequest
+from django.core.paginator import Paginator
 from django.db import models
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 
 from jobs.models import Opening
 
+PAGE_SIZE = 25
+
 
 def opening_list(request: HttpRequest):
-    openings = Opening.objects.all()
+    """Renders page listing openings.
+
+    :param request: HTTP request object
+    :type request: HttpRequest
+    """
+    page_no = request.GET.get('page', 1)
+    page_sz = request.GET.get('page_size', PAGE_SIZE)
+    search_term = request.GET.get('q')
+
+    openings = Opening.objects.order_by('id').all()
+    p = Paginator(openings, page_sz)
+    page = p.get_page(page_no)
+
+    # update querystring
+    qs = request.GET.copy()
+    if 'page' in qs:
+        del qs['page']
+
+    if 'q' in qs and not search_term:
+        del qs['q']
+
     return render(request, 'jobs/list.html', {
-        'openings': openings
+        'openings': page,
+        'q': search_term,
+        'qs': qs,
     })
 
 
 def opening_show(request: HttpRequest, opening_id: int):
+    """Renders the details page for an opening
+
+    :param request: HTTP request object
+    :type request: HttpRequest
+    :param opening_id: the opening id
+    :type opening_id: int
+    """
     opening = get_object_or_404(Opening, pk=opening_id)
 
     data = []
