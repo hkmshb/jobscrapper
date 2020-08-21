108cfcd452fe523cf58f7afb2f02df15fcfeb037
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-08-22 00:51:36 +0100

    add django-allauth and login.html form

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -48,16 +48,24 @@ ALLOWED_HOSTS = [ *get_env_value('ALLOWED_HOSTS', '').split(',') ]
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
-    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.postgres',
     'django.contrib.gis',
+
+    ## third-party
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    ## local
+    'core.apps.CoreConfig',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -147,7 +155,22 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+## Custom settings
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = 'core.User'
+
+## AllAuth
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'
